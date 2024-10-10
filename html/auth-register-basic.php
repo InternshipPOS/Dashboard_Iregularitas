87df12588d7b22c743ce6540ef8a271d278829ed
RizkyriaHutabarat <rizkyriahutabarat18@gmail.com>
2024-10-10 15:58:57 +0700

    push kiaaaa

--- html/auth-register-basic.php
+++ html/auth-register-basic.php
@@ -74,7 +74,7 @@ data-template="vertical-menu-template-free">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
     <title>Register - Pages | Iregularitas</title>
     <meta name="description" content="" />
-    <link rel="icon" type="image/x-icon" href="../assets/img/favicon/favicon.ico" />
+    <link rel="icon" type="image/x-icon" href="../assets/img/favicon/pos-favicon.png" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700&display=swap" rel="stylesheet" />
